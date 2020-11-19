d2c362d91e6d8213804894c1fde3cd63b7046ec8
FrancescoGrazioso <40018163+FrancescoGrazioso@users.noreply.github.com>
2020-11-19 20:59:41 +0100

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -23,14 +23,7 @@ export const mainURL = {
 };
 
 export const firebaseConfig = {
-  apiKey: 'AIzaSyC_e7Z1-oMsa4iu0I9Q-z5hGSxs4IiBLME',
-  authDomain: 'animeflix-1bfce.firebaseapp.com',
-  databaseURL: 'https://animeflix-1bfce.firebaseio.com',
-  projectId: 'animeflix-1bfce',
-  storageBucket: 'animeflix-1bfce.appspot.com',
-  messagingSenderId: '89220153623',
-  appId: '1:89220153623:web:69e362d583df3991dff752',
-  measurementId: 'G-C8VYF83CVQ'
+  
 };
 
 /*
